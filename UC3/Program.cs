21146258db7dea06c61ff77f36b5b25fe5882031
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-13 18:53:43 +0530

    Product View Management

---
+++ UC3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductReviewManagementWithLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product review management problem statement");
+
+            List<ProductReview> productReviewList = new List<ProductReview>()
+            {
+                new ProductReview(){ProductID=1,UserID=1,Rating=5,Review="Good",isLike=true},
+                new ProductReview(){ProductID=2,UserID=1,Rating=5,Review="Good",isLike=true},
+                new ProductReview(){ProductID=3,UserID=2,Rating=4,Review="Nice",isLike=true},
+                new ProductReview(){ProductID=4,UserID=2,Rating=2,Review="Good",isLike=true},
+                new ProductReview(){ProductID=5,UserID=3,Rating=3,Review="Nice",isLike=false},
+                new ProductReview(){ProductID=6,UserID=3,Rating=2,Review="Bad",isLike=false},
+                new ProductReview(){ProductID=7,UserID=4,Rating=5,Review="Good",isLike=true},
+                new ProductReview(){ProductID=8,UserID=3,Rating=1,Review="Bad",isLike=false},
+                new ProductReview(){ProductID=9,UserID=10,Rating=10,Review="Good",isLike=true},
+                new ProductReview(){ProductID=10,UserID=10,Rating=4,Review="Good",isLike=true},
+                new ProductReview(){ProductID=11,UserID=10,Rating=9,Review="Good",isLike=true},
+                new ProductReview(){ProductID=12,UserID=10,Rating=4,Review="Good",isLike=true},
+                new ProductReview(){ProductID=13,UserID=10,Rating=8,Review="Nice",isLike=true},
+                new ProductReview(){ProductID=14,UserID=10,Rating=4,Review="Good",isLike=true},
+                new ProductReview(){ProductID=15,UserID=10,Rating=4,Review="Good",isLike=true},
+                new ProductReview(){ProductID=16,UserID=10,Rating=4,Review="Good",isLike=true}
+            };
+
+            Management management = new Management();
+            management.SelectedRecords(productReviewList);
+        }
+    }
+}
