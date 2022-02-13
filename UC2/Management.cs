eac8c481be4a594b7600b83a8601e18ed5e09099
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-13 18:50:30 +0530

    Product Review Management

---
+++ UC2/Management.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace ProductReviewManagementWithLinq
+{
+    public class Management
+    {
+        public readonly DataTable dataTable = new DataTable();
+
+        public void TopRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = (from productReviews in listProductReview
+                                orderby productReviews.Rating descending
+                                select productReviews).Take(3);
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID: " + list.ProductID + "Userid: " + list.UserID + " Rating: " + list.Rating + "Review: " + list.Review + "isLike: " + list.isLike);
+
+            }
+        }
+    }
+}
