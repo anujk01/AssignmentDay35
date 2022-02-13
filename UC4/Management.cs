00e497b24cbcc4669f772d666dee52b397835888
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-13 18:55:36 +0530

    Product Review Management

---
+++ UC4/Management.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace ProductReviewManagementWithLinq
+{
+    public class Management
+    {
+        public readonly DataTable dataTable = new DataTable();
+
+        public void TopRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = (from productReviews in listProductReview
+                                orderby productReviews.Rating descending
+                                select productReviews).Take(3);
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID: " + list.ProductID + "Userid: " + list.UserID + " Rating: " + list.Rating + "Review: " + list.Review + "isLike: " + list.isLike);
+
+            }
+        }
+        
+        public void SelectedRecords(List<ProductReview> listProductReview)
+        {
+            var recordData = from productReviews in listProductReview
+                             where (productReviews.ProductID == 1 && productReviews.Rating > 3) ||
+                             (productReviews.ProductID == 4 && productReviews.Rating > 3 ) ||
+                             (productReviews.ProductID == 9 && productReviews.Rating > 3)
+                             select productReviews;
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("ProductID: " + list.ProductID + "Userid: " + list.UserID + " Rating: " + list.Rating + "Review: " + list.Review + "isLike: " + list.isLike);
+
+            }
+        }
+
+        public void RetrieveCountOfRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = listProductReview.GroupBy(x => x.ProductID).Select(x => new { ProductID = x.Key, Count = x.Count() });
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine(list.ProductID + "------" + list.Count);
+            }
+        }
+    }
+}
