3afd24c4d4949d58431dc20da80ff9d1530fcfb9
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-13 19:02:25 +0530

    Product Review Management

---
+++ UC7/Management.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace ProductReviewManagementWithLinq
+{
+    public class Management
+    {
+        public readonly DataTable dataTable = new DataTable();
+
+        public void RetrieveOnlyProductID(List<ProductReview> productList)
+        {
+            var recordedData = productList.Select(x => new { ProductId = x.ProductID, Review = x.Review });
+            Console.WriteLine("\nProductId and its review using LINQ select:");
+            foreach (var v in recordedData)
+            {
+                Console.WriteLine($"ProductID:{v.ProductId},ReviewCount:{v.Review}");
+            }
+        }
+    }
+}
